9b4113ffb1cb19e40abe36d0ea3fb93a3827cf4c
xyuaUi <xyuaui@gmail.com>
2026-03-13 02:49:18 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pwa-landscape-cache-v1';
-const ASSETS = ['.', 'index.html', 'manifest.json'];
+const CACHE_NAME = 'pwa-landscape-cache-v2';
+const ASSETS = ['.', 'index.html', 'manifest.json', 'assets/00.png'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
