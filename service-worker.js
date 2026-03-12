ed7e7f822c30122b2cc8c9b88ccbe865c65ef921
xyuaUi <xyuaui@gmail.com>
2026-03-13 02:40:49 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-btn-cache-v1';
+const CACHE_NAME = 'pwa-landscape-cache-v1';
 const ASSETS = ['.', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', e => {
@@ -9,4 +9,6 @@ self.addEventListener('activate', e => {
     e.waitUntil(caches.keys().then(names => Promise.all(names.map(n => n !== CACHE_NAME && caches.delete(n)))).then(() => self.clients.claim()));
 });
 
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
