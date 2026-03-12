1190e1623ba41d2d227059141b7014583611ffba
xyuaUi <xyuaui@gmail.com>
2026-03-13 01:03:36 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'pets-game-cache-v1';
+const ASSETS_TO_CACHE = [
+    '.',
+    'index.html',
+    'styles.css',
+    'app.js',
+    'assets/bg.jpg',
+    'assets/couch.png',
+    'assets/forum.png',
+    'assets/the-lamb.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
